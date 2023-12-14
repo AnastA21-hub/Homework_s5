855c5bd8ef49c21ec837821c06e07a3a551be904
Anatasia Andreeva <zemcova-anastasi@mail.ru>
2023-12-15 01:14:40 +0300

    Решена задача1 из семинара2

---
+++ Desktop/Gb_5515/seminar2/task1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход целое число из отрезка [10, 99] 
+//и показывает наибольшую цифру числа
+int number = new Random().Next(10, 100);
+Console.WriteLine(number);
+int num1 = number / 10;
+int num2 = number % 10;
+if (num1 > num2)
+{
+	Console.WriteLine(num1);
+}
+else
+{
+	Console.WriteLine(num2);
+}
