f19321ee2d6c4cc276e5c82f1abbeb63106d4eca
Anatasia Andreeva <zemcova-anastasi@mail.ru>
2023-12-15 01:06:16 +0300

    Решена задача5 из семинара2

---
+++ Desktop/Gb_5515/seminar2/task5/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая на вход принимает число и проверяет, кратно ли оно
+//одновременно 7 и 23 (да, нет)
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if ((num % 7 == 0) & (num % 23 == 0))
+{
+	Console.WriteLine("да");
+}
+else
+{
+	Console.WriteLine("нет");
+}
