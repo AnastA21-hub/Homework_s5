88dd4bb482494f27d2e3020c156ba45904a5a66d
Anatasia Andreeva <zemcova-anastasi@mail.ru>
2023-12-14 23:41:10 +0300

    Решена задача2 из семинара2

---
+++ Desktop/Gb_5515/seminar2/task2/Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите прогамму, которая принимает на вход трёхзначное число и удаляет вторую цифру этого числа
+int number = new Random().Next(100, 1000);
+Console.WriteLine(number);
+int num1 = number / 100;
+int num2 = number % 10;
+int result = num1 * 10 + num2;
+Console.WriteLine(result);
