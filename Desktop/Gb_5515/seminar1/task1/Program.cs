a28e30ce582bde719ec1cae77a4ff56fb0469533
Anatasia Andreeva <zemcova-anastasi@mail.ru>
2023-12-15 23:51:26 +0300

    Решили задачу1

---
+++ Desktop/Gb_5515/seminar1/task1/Program.cs
@@ -0,0 +1,6 @@
+﻿// Вход - число, выход - квадрат числа
+System.Console.WriteLine("Enter number: "); //сначала говорим, что нам нужно
+string str = Console.ReadLine();
+int num = Convert.ToInt32(str); //конвертер н вход принимает любое число, передаем ему строку, которую получили от пользователя (конвертировать будем из str)
+int squere = num * num; //теперь вычисляем квадрат числа (переменная int, которая обозначает squere - квадрат)
+System.Console.WriteLine($"squere = {squere}"); //Выводим квадрат числа
