897661fef7a14709c5f96ca52199584a1361dc22
Anatasia Andreeva <zemcova-anastasi@mail.ru>
2023-12-15 22:14:36 +0300

    Решили задачу10 из дз

---
+++ Desktop/Gb_5515/seminar2/task10/Program.cs
@@ -0,0 +1,13 @@
+﻿// Вход - трехзначное число, выход - вторая цифра этого числа
+Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if ((number > 99) & (number < 1000))
+{
+	int num = number / 10;
+	int num2 = num % 10;
+	Console.WriteLine(num2);
+}
+else
+{
+	Console.WriteLine("Некорректное число");
+}
