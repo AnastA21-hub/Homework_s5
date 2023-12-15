b2b3a27d04f572ebad43d0ab5703685fd8de7707
Anatasia Andreeva <zemcova-anastasi@mail.ru>
2023-12-16 00:01:08 +0300

    Решили задачу2

---
+++ Desktop/Gb_5515/seminar1/task2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Вход - два числа, выход - является ли первое квадратом второго
+Console.WriteLine("Enter number: ");
+string str1 = Console.ReadLine();
+Console.WriteLine("Enter number2: ");
+string str2 = Console.ReadLine();
+int num1 = Convert.ToInt32(str1);
+int num2 = Convert.ToInt32(str2);
+if (num2 * num2 == num1)// == это оператор сравнения
+{
+	Console.WriteLine("да");
+}
+else
+{
+	Console.WriteLine("нет");
+}
