57402a78b40dc31c900e018e3876a4a6aa9c61ae
Anatasia Andreeva <zemcova-anastasi@mail.ru>
2023-12-15 23:02:05 +0300

    Решили задачу13 из дз

---
+++ Desktop/Gb_5515/seminar2/task13/Program.cs
@@ -0,0 +1,24 @@
+﻿// вход - число. Выход - третья цифра заданного числа либо что ее нет
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if ((number > 99))
+{
+	if ((number > 99) & (number < 1000))
+	{
+		number %= 10;
+		Console.WriteLine(number);
+	}
+	while (number > 99)
+	{
+		number /= 10;
+		if ((number < 1000) & (number > 99))
+		{
+			number = number % 10;
+			Console.WriteLine(number);
+		}
+	}
+}
+else
+{
+	Console.WriteLine("третьей цифры нет");
+}
