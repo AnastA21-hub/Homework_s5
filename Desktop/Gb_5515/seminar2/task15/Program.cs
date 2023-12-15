d3dbdaeb3acb6a8fceb5565b33217e8fcf0a99e3
Anatasia Andreeva <zemcova-anastasi@mail.ru>
2023-12-15 22:28:37 +0300

    Решили задачу15 из дз

---
+++ Desktop/Gb_5515/seminar2/task15/Program.cs
@@ -0,0 +1,18 @@
+﻿// Вход - цифра дня недели, выход - является ли день выходным (да, нет)
+Console.WriteLine("Введите номер дня недели: ");
+int weekDay = Convert.ToInt32(Console.ReadLine());
+if (weekDay < 8)
+{
+	if ((weekDay == 6) || (weekDay == 7))
+	{
+		Console.WriteLine("да");
+	}
+	else
+	{
+		Console.WriteLine("нет");
+	}
+}
+else
+{
+	Console.WriteLine("Некорректное число");
+}
