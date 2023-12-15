a3bc28d90d11cc33c697c23e95cc8945c7d60b72
Anatasia Andreeva <zemcova-anastasi@mail.ru>
2023-12-16 00:10:18 +0300

    Решили задачу3

---
+++ Desktop/Gb_5515/seminar1/task3/Program.cs
@@ -0,0 +1,36 @@
+﻿// Вход - число, выход - название дня недели
+Console.WriteLine("Enter week day: ");
+string weekDay = Console.ReadLine();
+int numWeekDay = Convert.ToInt32(weekDay);
+if (numWeekDay == 1)
+{
+	Console.WriteLine("Понедельник");
+}
+else if (numWeekDay == 2)
+{
+	Console.WriteLine("Вторник");
+}
+else if (numWeekDay == 3)
+{
+	Console.WriteLine("Среда");
+}
+else if (numWeekDay == 4)
+{
+	Console.WriteLine("Четверг");
+}
+else if (numWeekDay == 5)
+{
+	Console.WriteLine("Пятница");
+}
+else if (numWeekDay == 6)
+{
+	Console.WriteLine("Суббота");
+}
+else if (numWeekDay == 7)
+{
+	Console.WriteLine("Воскресенье");
+}
+else
+{
+	Console.WriteLine("uncorrect week day");
+}
