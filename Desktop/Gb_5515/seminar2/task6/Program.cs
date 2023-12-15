85f2fadef441d067490ab52a2b38d493fc16d90d
Anatasia Andreeva <zemcova-anastasi@mail.ru>
2023-12-15 18:57:39 +0300

    Решили задачу6 из дз

---
+++ Desktop/Gb_5515/seminar2/task6/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая принимает на вход координаты точки (X и Y), 
+//причём X ≠ 0 и Y ≠ 0 и выдаёт номер координатной четверти плоскости, 
+//в которой находится эта точка.
+Console.WriteLine("Введите координату X: ");
+int X = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату Y: ");
+int Y = Convert.ToInt32(Console.ReadLine());
+if ((X != 0) & (Y != 0))
+{
+	if ((X > 0) & (Y > 0))
+	{
+		Console.WriteLine("1 координатная четверть");
+	}
+	if ((X < 0) & (Y > 0))
+	{
+		Console.WriteLine("2 координатная четверть");
+	}
+	if ((X < 0) & (Y < 0))
+	{
+		Console.WriteLine("3 координатная четверть");
+	}
+	if ((X > 0) & (Y < 0))
+	{
+		Console.WriteLine("4 координатная четверть");
+	}
+}
+else
+{
+	Console.WriteLine("Некорректные коодинаты");
+}
