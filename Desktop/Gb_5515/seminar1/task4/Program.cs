b02d58b8b44445ff0eccfde8294e72c49eea92aa
Anatasia Andreeva <zemcova-anastasi@mail.ru>
2023-12-16 00:35:02 +0300

    Решили задачи 4 и 5

---
+++ Desktop/Gb_5515/seminar1/task4/Program.cs
@@ -0,0 +1,10 @@
+﻿// Вход - целое число N, выход - все целые числа в промежутке от -N до N
+Console.WriteLine("Введите число: ");
+string str = Console.ReadLine();
+int num = Convert.ToInt32(str);
+int numStart = num * -1;
+while (numStart <= num)
+{
+	Console.WriteLine(numStart);
+	++numStart;//всё равно что numStart=numStart+1
+}
---
+++ Desktop/Gb_5515/seminar1/task5/Program.cs
@@ -0,0 +1,13 @@
+﻿// Вход - целое трёхзначное число, выход - последняя цифра этого числа
+Console.WriteLine("Введите число: ");
+string str = Console.ReadLine();
+int num = Convert.ToInt32(str);//задаем переменную
+if (num > 99 & num < 1000)//проверяем,является ли число трёхзначным
+{
+	int num2 = num % 10;//остаток целочисленного деления на 10
+	Console.WriteLine(num2);
+}
+else
+{
+	Console.WriteLine("некорректное число");
+}
