8457732280a25d22603887bcb5ad5b5039279678
Anatasia Andreeva <zemcova-anastasi@mail.ru>
2023-12-20 22:10:06 +0300

    готов

---
+++ Desktop/Gb_5515/seminar3/task1/Program.cs
@@ -0,0 +1,55 @@
+﻿/* Задайте программу, которая определяет, присутствут ли заданное число в массиве.
+Программа длжна выдавать ответ "да" или "нет".
+Т.е. Массив рандомных чисел, вводим число, результат "дА" или "нет"*/
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int[] list = new int[10];//задаем длину массива
+
+void FillArray(int[] collection)//метод "задаем массив"
+{
+	int length = collection.Length;
+	int index = 0;
+	while (index < length)
+	{
+		collection[index] = new Random().Next(1, 10);
+		index++;
+	}
+}
+
+void PrintArray(int[] col)//метод для печати массива
+{
+	int count = col.Length;
+	int position = 0;//создаем позицию для вывода элемента массива
+	while (position < count)
+	{
+		Console.Write(col[position] + " ");
+		position++;
+	}
+}
+
+bool FindNumber(int number, int[] array)//метод для поиска числа
+{
+	for (int i = 0; i < array.Length; i++)
+	{
+		if (number == array[i])
+		{
+			return true;
+		}
+	}
+	return false;
+}
+
+
+FillArray(list);//вызываем методпо рандомному заполнению массива
+PrintArray(list);//выводим массив
+
+Console.WriteLine();
+if (FindNumber(num, list))
+{
+	Console.WriteLine("Да");
+}
+else
+{
+	Console.WriteLine("Нет");
+}
---
+++ Desktop/Gb_5515/seminar3/task2/Program.cs
@@ -0,0 +1,37 @@
+﻿/*Здайте массив из 10-ти элементов, заполните числами из промежутка от -10 до 10.
+Замените отрицательные элементы на положительные, а положительные на отрицательные
+Пишем метод для нахождения массива*/
+void FillArray(int[] collection)//мето для нахождения массива
+{
+	int index = 0;
+	while (index < collection.Length)
+	{
+		collection[index] = new Random().Next(-10, 10);
+		index++;
+	}
+}
+
+void PrintArray(int[] col)// метод для вывода массива
+{
+	int position = 0;//иницилизируем переменную (исполняет роль индекса в печати), куда будут сохранятьcя элементы масива
+	while (position < col.Length)// пока выполняется условие, делай так
+	{
+		Console.Write(col[position] + " ");//выводим элемент массива в консоль
+		position++;//переходим к следующей позиции
+	}
+}
+
+void PositivArray(int[] pos)//создаем метод для изменения знака; в параметрах массив
+{
+	for (int i = 0; i < pos.Length; i++)//i - индекс
+	{
+		pos[i] *= -1;//элемент массва с индексом i умножаем на -1
+	}
+}
+
+int[] array = new int[10];//массиву array присваиваем новый массив длинной 10
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();//пустая строка между массивами
+PositivArray(array);
+PrintArray(array);
