b19103100f5a862c6757620a70c704187a7d3533
Anatasia Andreeva <zemcova-anastasi@mail.ru>
2023-12-21 00:09:37 +0300

    Задача2 решена

---
+++ Desktop/Homework_s3/task2/Program.cs
@@ -0,0 +1,31 @@
+﻿/*Задайте массив на 10 целых чисел. 
+Напишите программу, которая определяет количество чётных чисел в массиве.*/
+void FillPrintArray(int[] array)//метод для создания и печати массива
+{
+	int index = 0;
+	while (index < array.Length)
+	{
+		array[index] = new Random().Next(1, 100);
+		Console.Write(array[index] + " ");
+		index++;
+	}
+}
+
+int EvenNumbers(int[] array)
+{
+	int result = 0;
+	for (int i = 0; i < array.Length; i++)
+	{
+		if (array[i] % 2 == 0)
+		{
+			result++;
+		}
+	}
+	return result;
+}
+
+int[] array = new int[10];
+FillPrintArray(array);
+Console.WriteLine();
+int quantity = EvenNumbers(array);
+Console.WriteLine(quantity);
