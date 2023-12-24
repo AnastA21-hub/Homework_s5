74aa719f6b1e79ab671c23e81c4bfaf4ae6d8bc6
Anatasia Andreeva <zemcova-anastasi@mail.ru>
2023-12-24 21:51:57 +0300

    Решена задача1 ДЗ из семинара4

--- Desktop/Gb_5515/seminar4/task1hm/Program.cs
+++ Desktop/Gb_5515/seminar4/task1hm/Program.cs
@@ -1,24 +1,35 @@
 ﻿/*Напишите программу, которая бесконечно запрашивает целые числа с консоли. 
 Программа завершается при вводе символа ‘q’ или при вводе числа, 
 сумма цифр которого чётная.*/
-//сначала надо метод по определению числа условию
+//Решение не моё
 
 
-bool IsEven(int num)//сумма цифр числа четная
+while (true) // Бесконечный цикл
 {
-	int sum = (num % 10) + (num / 10);
-	if (sum % 2 == 0)
+	Console.Write("Введите число или 'q' для выхода: ");
+	string input = Console.ReadLine(); // Чтение строки ввода пользователя
+	if (input == "q") // Проверка на ввод 'q' для выхода
 	{
-		return false;
-		stop;
+		break;
 	}
-	return true;
-}
+	int number;
+	if (int.TryParse(input, out number)) // Проверка, является ли ввод числом{
+	{
+		int sum = 0;
 
-Console.WriteLine("Введите число: ");
-int number = Convert.ToInt32(Console.ReadLine());
-while (number = IsEven(number))
-{
-	Console.WriteLine("Введите число: ");
-	number = Convert.ToInt32(Console.ReadLine());
+		while (number > 0) // Вычисление суммы цифр числа
+		{
+			sum += number % 10; // Добавление последней цифры к сумме
+			number /= 10; // Удаление последней цифры из числа
+		}
+		if (sum % 2 == 0) // Проверка, является ли сумма цифр четной
+		{
+			Console.WriteLine("[STOP]");
+			break;
+		}
+	}
+	else // Если ввод не является числом и не 'q', повторить запрос
+	{
+		Console.WriteLine("Некорректный ввод. Пожалуйста, введите целое число или 'q'.");
+	}
 }
