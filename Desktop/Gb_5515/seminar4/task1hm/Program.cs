08f1d35a3b05105a5edd19b47ac7710c0dbe697a
Anatasia Andreeva <zemcova-anastasi@mail.ru>
2023-12-24 21:13:51 +0300

    Решена задача2 ДЗ из семинара4

---
+++ Desktop/Gb_5515/seminar4/task1hm/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Напишите программу, которая бесконечно запрашивает целые числа с консоли. 
+Программа завершается при вводе символа ‘q’ или при вводе числа, 
+сумма цифр которого чётная.*/
+//сначала надо метод по определению числа условию
+
+
+bool IsEven(int num)//сумма цифр числа четная
+{
+	int sum = (num % 10) + (num / 10);
+	if (sum % 2 == 0)
+	{
+		return false;
+		stop;
+	}
+	return true;
+}
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+while (number = IsEven(number))
+{
+	Console.WriteLine("Введите число: ");
+	number = Convert.ToInt32(Console.ReadLine());
+}
---
+++ Desktop/Gb_5515/seminar4/task2hm/Program.cs
@@ -0,0 +1,46 @@
+﻿/*Задайте массив заполненный случайными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.*/
+int[] FillArray(int size)//инициализируем массив в методе
+{
+	int[] numbers = new int[size];
+	for (int i = 0; i < numbers.Length; i++)
+	{
+		numbers[i] = new Random().Next(100, 1000);
+	}
+	return numbers;
+}
+
+void PrintArray(int[] num)//метод вывода массива в консоль
+{
+	for (int i = 0; i < num.Length; i++)
+	{
+		Console.Write(num[i] + " ");
+	}
+}
+
+bool IsEven(int num)// метод опрдления четного числа
+{
+	if (num % 2 == 0)
+	{
+		return true;
+	}
+	return false;
+}
+
+int GetCountEvenNumber(int[] array)
+{
+	int count = 0;
+	for (int i = 0; i < array.Length; i++)
+	{
+		if (IsEven(array[i]))
+		{
+			count++;
+		}
+	}
+	return count;
+}
+
+int[] list = FillArray(15);
+PrintArray(list);
+Console.WriteLine();
+Console.WriteLine(GetCountEvenNumber(list));
