48b0767d59af9ee08516a35e4aa9adf2dc3dc0d9
Anatasia Andreeva <zemcova-anastasi@mail.ru>
2023-12-27 22:32:05 +0300

    Решили задачу 2

---
+++ Desktop/Gb_5515/seminar5/task2/Program.cs
@@ -0,0 +1,48 @@
+﻿/*Задайте двумерный массив.
+Найдите сумму элементов, находящихся на главной диагонали.
+Главная диагональ - когда i=j (rows = columns)*/
+int[,] FillMatrix(int rows, int columns, int minValue, int maxValue)
+{
+	int[,] matrix = new int[rows, columns];
+	Random rand = new Random();
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < columns; j++)
+		{
+			matrix[i, j] = rand.Next(minValue, maxValue);
+		}
+	}
+	return matrix;
+}
+void PrintMatrix(int[,] matrix)
+{
+	for (int i = 0; i < matrix.GetLength(0); i++)
+	{
+		for (int j = 0; j < matrix.GetLength(1); j++)
+		{
+			Console.Write(matrix[i, j] + " ");
+		}
+		Console.WriteLine();
+	}
+}
+int SumNumbers(int[,] matrix)//метод для суммирования элементов главной диагонали
+{
+	int sum = 0;
+	for (int i = 0; i < matrix.GetLength(0); i++)
+	{
+		for (int j = 0; j < matrix.GetLength(1); j++)
+		{
+			if (i == j)
+			{
+				sum = sum + matrix[i, j];
+			}
+		}
+	}
+	return sum;
+}
+
+
+int[,] list = FillMatrix(5, 5, 0, 10);
+PrintMatrix(list);
+int sum = SumNumbers(list);
+Console.WriteLine(sum);
