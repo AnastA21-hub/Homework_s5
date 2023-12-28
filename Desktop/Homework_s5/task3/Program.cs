68190c9d6ca60c2b816ed2c39f47980a0212cc91
Anatasia Andreeva <zemcova-anastasi@mail.ru>
2023-12-28 20:57:03 +0300

    Решила задачу 3

---
+++ Desktop/Homework_s5/task3/Program.cs
@@ -0,0 +1,51 @@
+﻿/*Задайте прямоугольный двумерный массив. 
+Напишите программу, которая будет находить строку с наименьшей суммой элементов.*/
+int[,] FillMatrix(int rows, int columns, int minValue, int maxValue)
+{
+	int[,] matrix = new int[rows, columns];
+	Random rand = new Random();
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < columns; j++)
+		{
+			matrix[i, j] = rand.Next(minValue, maxValue);
+		}
+	}
+	return matrix;
+}
+
+void PrintMatrix(int[,] matrix)//метод для вывода двумерного массива
+{
+	for (int i = 0; i < matrix.GetLength(0); i++)
+	{
+		for (int j = 0; j < matrix.GetLength(1); j++)
+		{
+			Console.Write(matrix[i, j] + " ");
+		}
+		Console.WriteLine();
+	}
+}
+
+int[,] matrix = FillMatrix(5, 7, 10, 100);
+PrintMatrix(matrix);
+int FindMinNumber(int[,] matrix)//поиск минимального элеменка, выводим индекс строки
+{
+	int min = matrix[0, 0];
+	int rows = 0;
+	for (int i = 0; i < matrix.GetLength(0); i++)
+	{
+		for (int j = 0; j < matrix.GetLength(1); j++)
+		{
+			if (matrix[i, j] < min)
+			{
+				min = matrix[i, j];
+				rows = i;
+			}
+		}
+	}
+	return rows;
+}
+
+int index = FindMinNumber(matrix);
+
+System.Console.WriteLine($"Строка с индексом {index}");
