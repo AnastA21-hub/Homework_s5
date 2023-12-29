df32504333fe71cc45c37afd81333a95fe8545e5
Anatasia Andreeva <zemcova-anastasi@mail.ru>
2023-12-29 23:11:06 +0300

    Задача 1

---
+++ Desktop/Gb_5515/seminar6/task1/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Задайте массив символов тип []char, создайте строку из символов этого массива
+Укзание Конструктор строки вида string(char[]) не использовать
+Пример ['a', 'b', 'c', 'd'] => "abcd"*/
+
+
+using System.Text;
+{
+	char[] arr = { 'a', 'b', 'c', 'd' };//создаем массив тип char
+	string str = "";
+	StringBuilder strB = new StringBuilder();//этот класс выделяет память под себя
+	for (int i = 0; i < arr.Length; i++)
+	{
+		strB.Append(arr[i]);
+	}
+	System.Console.WriteLine(strB);
+}
+
