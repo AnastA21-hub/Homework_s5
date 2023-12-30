25f32ed3bca6817ed2fcf75b4db48df23e8e33fd
Anatasia Andreeva <zemcova-anastasi@mail.ru>
2023-12-30 12:55:04 +0300

    Задача 1

---
+++ Desktop/Gb_5515/seminar7/task1/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Напишите программу, которая на вход принимает число и возвращает сумму его цифр*/
+
+int GetNumber()//метод ввода числа с консоли
+{
+	Console.WriteLine("Введите число: ");
+	int number = Convert.ToInt32(Console.ReadLine());
+	return number;
+}
+
+int FindSum(int number)//считаем сумму чисел с помощью рекурсии
+{
+	if (number == 0)//базовый случай - после деления на 10 number = 0
+	{
+		return 0;
+	}
+	//если условие не выполняется, то сумма вычисляется
+	int sum = number % 10 + FindSum(number / 10);
+	return sum;
+}
+
+int number = GetNumber();
+int sum = FindSum(number);
+Console.WriteLine(sum);
