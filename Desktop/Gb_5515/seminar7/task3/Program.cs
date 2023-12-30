c5763ffec8c048b1165a31d0d7bf47e04bdce0e7
Anatasia Andreeva <zemcova-anastasi@mail.ru>
2023-12-30 21:21:08 +0300

    Задача 3

---
+++ Desktop/Gb_5515/seminar7/task3/Program.cs
@@ -0,0 +1,31 @@
+﻿/*Считйте из консоли строку, содержащую латинские буквы.
+Вывести на экран согласные буквы этой строки*/
+string GetString()
+{
+	Console.WriteLine("Введите строку: ");
+	string str = Console.ReadLine();
+	return str;
+}
+
+bool IsVowel(char c)//методпоиска гласных
+{
+	return c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U';
+	return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+}
+
+void PrintString(string str, int index = 0)
+{
+	if (index < str.Length)
+	{
+		char c = Char.ToUpper(str[index]);
+		if ((c >= 'A' && c <= 'Z') && !IsVowel(c))//! - превращает истину в ложь, а ложь в истину
+		{
+			Console.Write(c + " ");
+
+		}
+		PrintString(str, index + 1);
+	}
+}
+
+string str = GetString();
+PrintString(str);
